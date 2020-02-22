6b29f804bc1ed4605f8373b6ddb4a62d6c427f39
3larvas <3larvas@gmail.com>
2020-02-22 22:29:10 +0900

    red road filter

---
+++ opencv_05.py
@@ -0,0 +1,62 @@
+# 임계점 처리
+
+# import cv2
+# import matplotlib.pyplot as plt
+#
+# image = cv2.imread('test.jpg', cv2.IMREAD_GRAYSCALE)
+#
+# images = []
+# ret, thres1 = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+# ret, thres2 = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY_INV)
+# ret, thres3 = cv2.threshold(image, 127, 255, cv2.THRESH_TRUNC)
+# ret, thres4 = cv2.threshold(image, 127, 255, cv2.THRESH_TOZERO)
+# ret, thres5 = cv2.threshold(image, 127, 255, cv2.THRESH_TOZERO_INV)
+# images.append(thres1)
+# images.append(thres2)
+# images.append(thres3)
+# images.append(thres4)
+# images.append(thres5)
+#
+# for i in images:
+#   plt.imshow(cv2.cvtColor(i, cv2.COLOR_GRAY2RGB))
+#   plt.show()
+
+
+import cv2
+import matplotlib.pyplot as plt
+
+image = cv2.imread('test.jpg', cv2.IMREAD_GRAYSCALE)
+
+ret, thres1 = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+thres2 = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 21, 3)
+
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_GRAY2RGB))
+plt.show()
+
+plt.imshow(cv2.cvtColor(thres1, cv2.COLOR_GRAY2RGB))
+plt.show()
+
+plt.imshow(cv2.cvtColor(thres2, cv2.COLOR_GRAY2RGB))
+plt.show()
+#
+# import cv2
+# import numpy as np
+#
+#
+# def change_color(x):
+#   r = cv2.getTrackbarPos("R", "Image")
+#   g = cv2.getTrackbarPos("G", "Image")
+#   b = cv2.getTrackbarPos("B", "Image")
+#   image[:] = [b, g, r]
+#   cv2.imshow('Image', image)
+#
+#
+# image = np.zeros((600, 400, 3), np.uint8)
+# cv2.namedWindow("Image")
+#
+# cv2.createTrackbar("R", "Image", 0, 255, change_color)
+# cv2.createTrackbar("G", "Image", 0, 255, change_color)
+# cv2.createTrackbar("B", "Image", 0, 255, change_color)
+#
+# cv2.imshow('Image', image)
+# cv2.waitKey(0)
---
+++ opencv_06.py
@@ -0,0 +1,40 @@
+# import cv2
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# image = cv2.imread('test.jpg')
+# plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+# plt.show()
+#
+# size = 4
+# kernel = np.ones((size, size), np.float32) / (size ** 2)
+# print(kernel)
+#
+# dst = cv2.filter2D(image, -1, kernel)
+# plt.imshow(cv2.cvtColor(dst, cv2.COLOR_BGR2RGB))
+# plt.show()
+
+
+# import cv2
+# import matplotlib.pyplot as plt
+#
+# image = cv2.imread('test.jpg')
+# plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+# plt.show()
+#
+# dst = cv2.blur(image, (4, 4))
+# plt.imshow(cv2.cvtColor(dst, cv2.COLOR_BGR2RGB))
+# plt.show()
+
+
+import cv2
+import matplotlib.pyplot as plt
+
+image = cv2.imread('test.jpg')
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.show()
+
+# kernel_size: 홀수
+dst = cv2.GaussianBlur(image, (7, 7), 0)
+plt.imshow(cv2.cvtColor(dst, cv2.COLOR_BGR2RGB))
+plt.show()
