409502481d272063dde3aaa8238f05a588047f08
3larvas <3larvas@gmail.com>
2020-02-24 21:35:45 +0900

    일정범위에서 빨간색으로 인식되는 부분이 몇 퍼센트인지 출력

--- video_read.py
+++ video_read.py
@@ -1,12 +1,26 @@
 import cv2
 import numpy as np
+L_LINE_UP_COL = 240
+L_LINE_UP_ROW = 255
+L_LINE_DN_COL = 160
+L_LINE_DN_ROW = 300
 
-cap = cv2.VideoCapture('red_road_04.mp4')
+R_LINE_UP_COL = 380
+R_LINE_UP_ROW = 255
+R_LINE_DN_COL = 460
+R_LINE_DN_ROW = 300
+
+
+
+cap = cv2.VideoCapture('rsc/red_road_03.mp4')
 
 while(cap.isOpened()):
     ret, frame = cap.read()
     frame = cv2.resize(frame, (640, 360))
     if ret:
+        # 현재 차선 가이드라인(고정)
+        frame = cv2.line(frame, (L_LINE_UP_COL, L_LINE_UP_ROW), (L_LINE_DN_COL, L_LINE_DN_ROW), (255, 0, 0), 3)
+        frame = cv2.line(frame, (R_LINE_UP_COL, R_LINE_UP_ROW), (R_LINE_DN_COL, R_LINE_DN_ROW), (255, 0, 0), 3)
         # BGR->HSV로 변환
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -21,6 +35,15 @@ while(cap.isOpened()):
         mask_1 = cv2.inRange(hsv, lower_red_1, upper_red_1)
         mask_2 = cv2.inRange(hsv, lower_red_2, upper_red_2)
         result_mask = cv2.add(mask_1, mask_2)
+        cnt = 0
+        cnt_255 = 0
+        for i in range(240, 380):
+            for j in range(255, 300):
+                cnt += 1
+                if(result_mask[j][i]==255): cnt_255 += 1
+        msg =  str(round(cnt_255 * 100 / cnt,2)) + "%"
+        cv2.putText(result_mask, msg, (30, 30), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 2)
+
 
         # bit연산자를 통해서 red영역만 남김.
         res = cv2.bitwise_and(frame, frame, mask=result_mask)
