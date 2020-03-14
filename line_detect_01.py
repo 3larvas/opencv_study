fa6602ed9424f40084716875314d65a214462fca
3larvas <3larvas@gmail.com>
2020-03-14 15:23:46 +0900

    차선 인식 후 각도 계산

--- line_detect_01.py
+++ line_detect_01.py
@@ -1,11 +1,34 @@
 import cv2
 import numpy as np
+import math
 
 def roi(equ_frame, vertices):
     mask = np.zeros_like(equ_frame)
     cv2.fillPoly(mask, vertices, 255)
     masked = cv2.bitwise_and(equ_frame, mask)
     return masked
+
+def nothing(x):
+    pass
+
+def initializeTrackbars(intialTracbarVals):
+    cv2.namedWindow("Trackbars")
+    cv2.resizeWindow("Trackbars", 360, 240)
+    cv2.createTrackbar("Width Top", "Trackbars", intialTracbarVals[0],50, nothing)
+    cv2.createTrackbar("Height Top", "Trackbars", intialTracbarVals[1], 100, nothing)
+    cv2.createTrackbar("Width Bottom", "Trackbars", intialTracbarVals[2], 50, nothing)
+    cv2.createTrackbar("Height Bottom", "Trackbars", intialTracbarVals[3], 100, nothing)
+
+def valTrackbars():
+    widthTop = cv2.getTrackbarPos("Width Top", "Trackbars")
+    heightTop = cv2.getTrackbarPos("Height Top", "Trackbars")
+    widthBottom = cv2.getTrackbarPos("Width Bottom", "Trackbars")
+    heightBottom = cv2.getTrackbarPos("Height Bottom", "Trackbars")
+
+    src = np.float32([(widthTop/100,heightTop/100), (1-(widthTop/100), heightTop/100),
+                      (widthBottom/100, heightBottom/100), (1-(widthBottom/100), heightBottom/100)])
+    #src = np.float32([(0.43, 0.65), (0.58, 0.65), (0.1, 1), (1, 1)])
+    return src
 
 def perspective_warp(img,
                      dst_size=(1280, 720),
@@ -21,35 +44,27 @@ def perspective_warp(img,
     M = cv2.getPerspectiveTransform(src, dst)
     # Warp the image using OpenCV warpPerspective()
     warped = cv2.warpPerspective(img, M, dst_size)
-
     return warped
 
+intialTracbarVals = [25,60,1,70]   #wT,hT,wB,hB
 video = cv2.VideoCapture("rsc/mission_left_right.mp4")
-# video = cv2.VideoCapture("rsc/mission_bus_lane.mp4")
-# video = cv2.VideoCapture("rsc/mission_full.mp4")
+
+initializeTrackbars(intialTracbarVals)
 
 while True:
+    perspective_val = valTrackbars()
     ret, orig_frame = video.read()
-
     frame = cv2.GaussianBlur(orig_frame,(5, 5), 0)
     height, width = frame.shape[:2]
 
-    widthTop = 28
-    heightTop = 60
-    widthBottom = 1
-    heightBottom = 80
-    perspective_val = src = np.float32([(widthTop / 100, heightTop / 100), (1 - (widthTop / 100), heightTop / 100),
-                                        (widthBottom / 100, heightBottom / 100),
-                                        (1 - (widthBottom / 100), heightBottom / 100)])
     frame = perspective_warp(frame, dst_size=(width, height), src=perspective_val)
     # line_img_frame = perspective_warp(frame, dst_size=(width, height), src=perspective_val)
-
     frame = cv2.pyrDown(frame)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # white line detect
     low_w = np.array([0, 0, 130])
-    up_w = np.array([255, 80, 255])
+    up_w = np.array([255, 50, 255])
     w_mask = cv2.inRange(hsv, low_w, up_w)
     w_edges = cv2.Canny(w_mask, 100, 200)
 
@@ -60,70 +75,73 @@ while True:
     y_edges = cv2.Canny(y_mask, 100, 200)
 
     # blue line detect
-    low_b = np.array([120, 130, 70])
+    low_b = np.array([120, 70, 70])
     up_b = np.array([170, 170, 120])
     b_mask = cv2.inRange(hsv, low_b, up_b)
     b_edges = cv2.Canny(b_mask, 100, 200)
 
     # set ROI
-    # vertices1 = np.array([[(0 , height*0.8), (width * 0.3 , height * 0.6), (width * 0.5, height * 0.6), (width * 0.3, height)]], dtype=np.int32)
-    # vertices2 = np.array([[(width * 0.7, height), (width * 0.5, height * 0.6), (width * 0.7, height * 0.6), (width, height*0.8)]],dtype=np.int32)
-    vertices1 = np.array([[(0 , height), (0, 0),(width * 0.5, 0), (width * 0.3, height)]], dtype=np.int32)
-    vertices2 = np.array([[(width, 0), (width, height), (width * 0.7, height), (width*0.5, 0)]],dtype=np.int32)
-    w_roi_frame1 = roi(w_edges, [vertices1])
-    w_roi_frame2 = roi(w_edges, [vertices2])
-    y_roi_frame1 = roi(y_edges, [vertices1])
-    y_roi_frame2 = roi(y_edges, [vertices2])
-    b_roi_frame1 = roi(b_edges, [vertices1])
-    b_roi_frame2 = roi(b_edges, [vertices2])
-    w_roi_frame = cv2.add(w_roi_frame1, w_roi_frame2)
-    w_roi_frame = np.uint8(w_roi_frame)
-    y_roi_frame = cv2.add(y_roi_frame1, y_roi_frame2)
-    y_roi_frame = np.uint8(y_roi_frame)
-    b_roi_frame = cv2.add(b_roi_frame1, b_roi_frame2)
-    b_roi_frame = np.uint8(b_roi_frame)
+    roi_height, roi_width = w_edges.shape[:2]
 
     # draw white lines
-    w_lines = cv2.HoughLinesP(w_roi_frame, 1, np.pi/180, 50, maxLineGap=50)
+    w_lines = cv2.HoughLinesP(w_edges, 1, np.pi/180, 50, maxLineGap=50)
+    sum_angle = 0
+    cnt_angle =0
     if w_lines is not None:
         for line in w_lines:
             x1, y1, x2, y2 = line[0]
-            if(abs(y1-y2) > height * 0.1  and abs(x1-x2)<width*0.5) :
+            if(abs(y1-y2) > roi_height * 0.45  and abs(x1-x2)<roi_width*0.5) :
                 cv2.line(frame, (x1, y1), (x2, y2), (255, 255, 255), 5)
-            elif(abs(y1-y2) < height * 0.025  and abs(x1-x2)>width*0.25) :
+                tmp_angle = math.degrees(math.atan2(x2 - x1, y2 - y1))
+                if(90 <= tmp_angle and tmp_angle <= 180) : tmp_angle -= 180
+                sum_angle += tmp_angle * (-1)
+                cnt_angle += 1
+            elif(abs(y1-y2) < roi_height * 0.1  and abs(x1-x2)>roi_width*0.25) :
                 cv2.line(frame, (x1, y1), (x2, y2), (25, 25, 255), 5)
 
     # draw yellow lines
-    y_lines = cv2.HoughLinesP(y_roi_frame, 1, np.pi / 180, 50, maxLineGap=50)
+    y_lines = cv2.HoughLinesP(y_edges, 1, np.pi / 180, 50, maxLineGap=50)
     if y_lines is not None:
         for line in y_lines:
             x1, y1, x2, y2 = line[0]
-            if (abs(y1 - y2) > height * 0.1 and abs(x1 - x2) < width * 0.4):
+            if (abs(y1 - y2) > roi_height * 0.1 and abs(x1 - x2) < roi_width * 0.4):
                 cv2.line(frame, (x1, y1), (x2, y2), (51, 204, 255), 5)
+                tmp_angle = math.degrees(math.atan2(x2 - x1, y2 - y1))
+                if (90 <= tmp_angle and tmp_angle <= 180): tmp_angle -= 180
+                sum_angle += tmp_angle * (-1)
+                cnt_angle += 1
 
     # draw blue lines
-    b_lines = cv2.HoughLinesP(b_roi_frame, 1, np.pi / 180, 50, maxLineGap=50)
+    b_lines = cv2.HoughLinesP(b_edges, 1, np.pi / 180, 50, maxLineGap=50)
     if b_lines is not None:
         for line in b_lines:
             x1, y1, x2, y2 = line[0]
-            if (abs(y1 - y2) > height * 0.1 and abs(x1 - x2) < width * 0.5):
+            if (abs(y1 - y2) > roi_height * 0.1 and abs(x1 - x2) < roi_width * 0.5):
                 cv2.line(frame, (x1, y1), (x2, y2), (255, 24, 25), 5)
+                tmp_angle = math.degrees(math.atan2(x2 - x1, y2 - y1))
+                if (90 <= tmp_angle and tmp_angle <= 180): tmp_angle -= 180
+                sum_angle += tmp_angle * (-1)
+                cnt_angle += 1
+
+    # calculate angle
+    if(cnt_angle!=0) :
+        print(round(sum_angle / cnt_angle, 2))
+        msg = str(round(sum_angle / cnt_angle, 2)) + "'"
+        cv2.putText(frame, msg , (30, 30), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 2)
 
     #show img
     cv2.imshow("lineframe", frame)
-    cv2.imshow("w_roi_frame", w_roi_frame)
-    cv2.imshow("y_roi_frame", y_roi_frame)
-    cv2.imshow("b_roi_frame", b_roi_frame)
+    cv2.imshow("w_roi_frame", w_edges)
+    cv2.imshow("y_roi_frame", y_edges)
+    cv2.imshow("b_roi_frame", b_edges)
 
-    roi_frame = cv2.add(w_roi_frame, y_roi_frame)
-    roi_frame = cv2.add(roi_frame, b_roi_frame)
+    roi_frame = cv2.add(w_edges, y_edges)
+    roi_frame = cv2.add(roi_frame, b_edges)
     cv2.imshow("roi_frame", roi_frame)
+    # cv2.imshow("ori_frame", roi_ori_frame)
 
-
-    # cv2.imshow("perspective_frame", img_frame)
-    # cv2.imshow("line_perspective_frame", line_img_frame)
     key = cv2.waitKey(1)
     if key == 27:
         break
 video.release()
-cv2.destroyAllWindows() 
+cv2.destroyAllWindows()
