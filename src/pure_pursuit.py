37553047dfd664efdc5f45e192429328de01a23f
3larvas <3larvas@gmail.com>
2020-04-28 22:11:20 +0900

    pure pursuit test

---
+++ src/pure_pursuit.py
@@ -0,0 +1,411 @@
+"""
+Path tracking simulation with pure pursuit steering and PID speed control.
+author: Atsushi Sakai (@Atsushi_twi)
+        Guillaume Jacquenot (@Gjacquenot)
+"""
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import sys
+import time
+import serial
+import threading
+import queue
+
+# Parameters
+k = 0.1  # look forward gain
+Lfc = 4.0  # [m] look-ahead distance
+Kp = 1.0  # speed proportional gain
+dt = 0.05  # [s] time tick
+WB = 0.9  # [m] wheel base of vehicle
+
+show_animation = True
+
+
+class State:
+
+    def __init__(self, x=0.0, y=0.0, yaw=0.0, v=0.0):
+        self.x = x
+        self.y = y
+        self.yaw = yaw
+        self.v = v
+        self.rear_x = self.x - ((WB / 2) * math.cos(self.yaw))
+        self.rear_y = self.y - ((WB / 2) * math.sin(self.yaw))
+
+    def update(self, a, delta):
+        self.x += self.v * math.cos(self.yaw) * dt
+        self.y += self.v * math.sin(self.yaw) * dt
+        self.yaw += self.v / WB * math.tan(delta) * dt
+        self.v += a * dt
+        self.rear_x = self.x - ((WB / 2) * math.cos(self.yaw))
+        self.rear_y = self.y - ((WB / 2) * math.sin(self.yaw))
+
+    def calc_distance(self, point_x, point_y):
+        dx = self.rear_x - point_x
+        dy = self.rear_y - point_y
+        return math.hypot(dx, dy)
+
+
+class States:
+
+    def __init__(self):
+        self.x = []
+        self.y = []
+        self.yaw = []
+        self.v = []
+        self.t = []
+
+    def append(self, t, state):
+        self.x.append(state.x)
+        self.y.append(state.y)
+        self.yaw.append(state.yaw)
+        self.v.append(state.v)
+        self.t.append(t)
+
+
+def proportional_control(target, current):
+    a = Kp * (target - current)
+
+    return a
+
+
+class TargetCourse:
+
+    def __init__(self, cx, cy):
+        self.cx = cx
+        self.cy = cy
+        self.old_nearest_point_index = None
+
+    def search_target_index(self, state):
+
+        # To speed up nearest point search, doing it at only first time.
+        if self.old_nearest_point_index is None:
+            # search nearest point index
+            dx = [state.rear_x - icx for icx in self.cx]
+            dy = [state.rear_y - icy for icy in self.cy]
+            d = np.hypot(dx, dy)
+            ind = np.argmin(d)
+            self.old_nearest_point_index = ind
+        else:
+            ind = self.old_nearest_point_index
+            distance_this_index = state.calc_distance(self.cx[ind],
+                                                      self.cy[ind])
+            while True:
+                distance_next_index = state.calc_distance(self.cx[ind + 1],
+                                                          self.cy[ind + 1])
+                if distance_this_index < distance_next_index:
+                    break
+                ind = ind + 1 if (ind + 1) < len(self.cx) else ind
+                distance_this_index = distance_next_index
+            self.old_nearest_point_index = ind
+
+        Lf = k * state.v + Lfc  # update look ahead distance
+
+        # search look ahead target point index
+        while Lf > state.calc_distance(self.cx[ind], self.cy[ind]):
+            if (ind + 1) >= len(self.cx):
+                break  # not exceed goal
+            ind += 1
+
+        return ind, Lf
+
+
+def pure_pursuit_steer_control(state, trajectory, pind):
+    ind, Lf = trajectory.search_target_index(state)
+
+    if pind >= ind:
+        ind = pind
+
+    if ind < len(trajectory.cx):
+        tx = trajectory.cx[ind]
+        ty = trajectory.cy[ind]
+    else:  # toward goal
+        tx = trajectory.cx[-1]
+        ty = trajectory.cy[-1]
+        ind = len(trajectory.cx) - 1
+
+    alpha = math.atan2(ty - state.rear_y, tx - state.rear_x) - state.yaw
+
+    delta = math.atan2(2.0 * WB * math.sin(alpha) / Lf, 1.0)
+    # print(delta)
+    return delta, ind
+
+
+def plot_arrow(x, y, yaw, length=1.0, width=0.5, fc="r", ec="k"):
+    """
+    Plot arrow
+    """
+
+    if not isinstance(x, float):
+        for ix, iy, iyaw in zip(x, y, yaw):
+            plot_arrow(ix, iy, iyaw)
+    else:
+        plt.arrow(x, y, length * math.cos(yaw), length * math.sin(yaw),
+                  fc=fc, ec=ec, head_width=width, head_length=width)
+        plt.plot(x, y)
+
+
+class Protocol(object):
+    """\
+    Protocol as used by the ReaderThread. This base class provides empty
+    implementations of all methods.
+    """
+
+    def connection_made(self, transport):
+        """Called when reader thread is started"""
+
+    def data_received(self, data):
+        """Called with snippets received from the serial port"""
+
+    def connection_lost(self, exc):
+        """\
+        Called when the serial port is closed or the reader loop terminated
+        otherwise.
+        """
+        if isinstance(exc, Exception):
+            raise exc
+
+
+class ReaderThread(threading.Thread):
+    """\
+    Implement a serial port read loop and dispatch to a Protocol instance (like
+    the asyncio.Protocol) but do it with threads.
+    Calls to close() will close the serial port but it is also possible to just
+    stop() this thread and continue the serial port instance otherwise.
+    """
+
+    def __init__(self, serial_instance, protocol_factory):
+        """\
+        Initialize thread.
+        Note that the serial_instance' timeout is set to one second!
+        Other settings are not changed.
+        """
+        super(ReaderThread, self).__init__()
+        self.daemon = True
+        self.serial = serial_instance
+        self.protocol_factory = protocol_factory
+        self.alive = True
+        self._lock = threading.Lock()
+        self._connection_made = threading.Event()
+        self.protocol = None
+
+    def stop(self):
+        """Stop the reader thread"""
+        self.alive = False
+        if hasattr(self.serial, 'cancel_read'):
+            self.serial.cancel_read()
+        self.join(2)
+
+    def run(self):
+        """Reader loop"""
+        if not hasattr(self.serial, 'cancel_read'):
+            self.serial.timeout = 1
+        self.protocol = self.protocol_factory()
+        try:
+            self.protocol.connection_made(self)
+        except Exception as e:
+            self.alive = False
+            self.protocol.connection_lost(e)
+            self._connection_made.set()
+            return
+        error = None
+        self._connection_made.set()
+        while self.alive and self.serial.is_open:
+            try:
+                # read all that is there or wait for one byte (blocking)
+                data = self.serial.read(self.serial.in_waiting or 1)
+            except serial.SerialException as e:
+                # probably some I/O problem such as disconnected USB serial
+                # adapters -> exit
+                error = e
+                break
+            else:
+                if data:
+                    # make a separated try-except for called used code
+                    try:
+                        self.protocol.data_received(data)
+                    except Exception as e:
+                        error = e
+                        break
+        self.alive = False
+        self.protocol.connection_lost(error)
+        self.protocol = None
+
+    def write(self, data):
+        """Thread safe writing (uses lock)"""
+        self.serial.write(data)
+        # with self._lock:
+        #
+        #     print(data)
+            # self.serial.write(data)
+
+    def close(self):
+        """Close the serial port and exit reader thread (uses lock)"""
+        # use the lock to let other threads finish writing
+        with self._lock:
+            # first stop reading, so that closing can be done on idle port
+            self.stop()
+            self.serial.close()
+
+    def connect(self):
+        """
+        Wait until connection is set up and return the transport and protocol
+        instances.
+        """
+        if self.alive:
+            self._connection_made.wait()
+            if not self.alive:
+                raise RuntimeError('connection_lost already called')
+            return (self, self.protocol)
+        else:
+            raise RuntimeError('already stopped')
+
+    # - -  context manager, returns protocol
+
+    def __enter__(self):
+        """\
+        Enter context handler. May raise RuntimeError in case the connection
+        could not be created.
+        """
+        self.start()
+        self._connection_made.wait()
+        if not self.alive:
+            raise RuntimeError('connection_lost already called')
+        return self.protocol
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Leave context: close port"""
+        self.close()
+
+
+# 프로토콜
+class rawProtocal(Protocol):
+    # 연결 시작시 발생
+    def connection_made(self, transport):
+        self.transport = transport
+        self.running = True
+
+    # 연결 종료시 발생
+    def connection_lost(self, exc):
+        self.transport = None
+
+    #데이터가 들어오면 이곳에서 처리함.
+    def data_received(self, data):
+        print('recv : ', data)
+
+    # 데이터 보낼 때 함수
+    def write(self,data):
+        print('send : ', data)
+        self.transport.write(data)
+
+    # 종료 체크
+    def isDone(self):
+        return self.running
+
+
+
+def main():
+    #  target course
+    # cx = np.arange(0, 50, 0.5)
+    # cy = [math.sin(ix / 3.0) * ix / 2.0 for ix in cx]
+    
+    # 경로 정보 입력
+    cx = [0, 5, 10, 30, 31]
+    cy = [0, 10, -15, 0, 0]
+    #목표 속도 설정
+    target_speed = 10.0 / 3.6  # [m/s]
+
+    T = 100.0  # max simulation time
+
+    # 시작 지점 설정
+    state = State(x=-0.0, y=-6.0, yaw=180.0, v=0.0)
+    
+    #현재 지점 기준 way point를 지나쳤는지 검사
+    lastIndex = len(cx) - 1
+    time = 0.0
+    states = States()
+    states.append(time, state)
+    target_course = TargetCourse(cx, cy)
+    target_ind, _ = target_course.search_target_index(state)
+
+    # 포트 설정
+    PORT = 'COM2'
+    # 연결
+    ser = serial.serial_for_url(PORT, baudrate=115200, timeout=1)
+
+    while T >= time and lastIndex > target_ind:
+
+        # Calc control input
+        ai = proportional_control(target_speed, state.v)
+        di, target_ind = pure_pursuit_steer_control(
+            state, target_course, target_ind)
+
+        state.update(ai, di)  # Control vehicle
+
+        time += dt
+        states.append(time, state)
+        ## -------------------- 시리얼 통신 -------------------- ##
+        # 쓰레드 시작
+        speed_low    = int(state.v * 3.6) & 0xff
+        speed_high   = int(state.v * 3.6) >> 8
+        steer_low    = int(di*3000)+1500 & 0xff
+        steer_high   = int(di*3000)+1500 >> 8
+        packet = bytearray()
+        packet.append(0x53)  # msg_s #변경 X
+        packet.append(0x54)  # msg_t #변경 X
+        packet.append(0x58)  # msg_x #변경 X
+        packet.append(0x00)  #
+        print("spd_high : ", speed_high , "spd_low : " , speed_low)
+        packet.append(speed_high)  # msg_speed_
+        packet.append(speed_low)  # msg_speed_
+        print("str : ",int(di*3000)+1500,"str_high : ", steer_high, "str_low : ", steer_low)
+        packet.append(steer_high)  # msg_steer_
+        packet.append(steer_low)  # msg_steer_
+        packet.append(0xDC)  # mag_break
+        packet.append(0x05)  # msg_alive #변경 X
+        packet.append(0x00)  # msg_alive #변경 X
+        packet.append(0x0D)  # msg_etx_0 #변경 X
+        packet.append(0x0A)  # msg_etx_1 #변경 X
+        # with ReaderThread(ser, rawProtocal) as p:
+        ser.write(packet)
+        ## ------------------------------------------------------ ##
+        if show_animation:  # pragma: no cover
+            plt.cla()
+            # for stopping simulation with the esc key.
+            plt.gcf().canvas.mpl_connect(
+                'key_release_event',
+                lambda event: [exit(0) if event.key == 'escape' else None])
+            plot_arrow(state.x, state.y, state.yaw)
+            plt.plot(cx, cy, "-r", label="course")
+            plt.plot(states.x, states.y, "-b", label="trajectory")
+            plt.plot(cx[target_ind], cy[target_ind], "xg", label="target")
+            plt.axis("equal")
+            plt.grid(True)
+            plt.title("Speed[km/h]:" + str(state.v * 3.6)[:4])
+            plt.pause(0.001)
+
+    # Test
+    assert lastIndex >= target_ind, "Cannot goal"
+
+    if show_animation:  # pragma: no cover
+        plt.cla()
+        plt.plot(cx, cy, ".r", label="course")
+        plt.plot(states.x, states.y, "-b", label="trajectory")
+        plt.legend()
+        plt.xlabel("x[m]")
+        plt.ylabel("y[m]")
+        plt.axis("equal")
+        plt.grid(True)
+
+        plt.subplots(1)
+        plt.plot(states.t, [iv * 3.6 for iv in states.v], "-r")
+        plt.xlabel("Time[s]")
+        plt.ylabel("Speed[km/h]")
+        plt.grid(True)
+        plt.show()
+
+
+if __name__ == '__main__':
+    print("Pure pursuit path tracking simulation start")
+    main()
+
